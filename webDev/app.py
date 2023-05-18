a4cf303ac02f4b0edae7bd68ce44b16a64924b5b
Rhina Kim <chihiroanihr@gmail.com>
2023-05-18 07:25:34 -0400

    update

--- webDev/app.py
+++ webDev/app.py
@@ -1,6 +1,5 @@
 from flask import Flask, render_template, request, send_from_directory
 import cv2
-import keras
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, Conv2D, MaxPooling2D, BatchNormalization, Flatten
 import numpy as np
